eb9d60c32a1866575084feee027e21b926e36c0b
Tiago Henrique <Tiago Henrique@192.168.25.16>
2016-11-16 22:06:15 -0200

    Alterado nome da variavel

--- app/connection/controller.php
+++ app/connection/controller.php
@@ -1,9 +1,9 @@
 <?php
 $titulo = "Conexão com o Banco de Dados";
 
-$conexao = @mysqli_connect ( $db_host, $db_user, $db_pass, $db_name );
+$connection = @mysqli_connect ( $db_host, $db_user, $db_pass, $db_name );
 
-if (mysqli_connect_errno ( $conexao )) {
+if (mysqli_connect_errno ( $connection )) {
 	$resultado = "Algum erro ocorreu com a conexão do servidor de 
 			banco de dados, erro reportado: " . mysqli_connect_error ();
 }
