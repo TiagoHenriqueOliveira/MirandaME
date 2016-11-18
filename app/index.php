4e838157896f229c165ad9150f36edcc0082b0ae
Tiago Henrique <Tiago Henrique@192.168.25.16>
2016-11-17 23:24:00 -0200

    Refatorado código

--- app/index.php
+++ app/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$db_host = "localhost";
+$db_host = "localhost:3306";
 $db_user = "root";
 $db_pass = "";
 $db_name = "miranda_me";
@@ -9,8 +9,3 @@ $db_name = "miranda_me";
 $route = $_GET['route'];
 
 require_once($route."/index.php");
-
-// rainTPL
-include("lib/template/raintpl/rain.tpl.class.php");
-raintpl::$tpl_dir = $_GET['route']."/tpl/"; // template directory
-raintpl::$cache_dir = $_GET['route']."/tmp/"; // cache directory
