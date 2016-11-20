58ce515552a92234cef3495b29d462e1c360b484
Tiago Henrique <Tiago Henrique@192.168.25.16>
2016-11-20 20:10:25 -0200

    Corrigido listagem

--- app/local/view_lista_local.php
+++ app/local/view_lista_local.php
@@ -9,20 +9,20 @@
 	<?php if(isset($return)) { ?>
 		<h2><?=$return?></h2>
 	<?php } ?>
-	<h3><?=$resultado?></h3>
 	<p><a href="index.php?route=local&action=cadastrar">Cadastrar Local</a></p>
 	
 	<table border="1" cellpadding="5" cellspacing="5" width="550">
 			<tr>
 				<td>ID</td>
 				<td>Nome</td>
-				<td>Idade</td>
+				<td>Responsável</td>
 			</tr>
 			<?php foreach($locais as $local) { ?>
 				<tr>
 					<td><?=$local['id']?></td>
 					<td><?=$local['nome']?></td>
-					<td><?=$local['idade']?></td>
+					<td><?=$local['responsavel']?></td>
+					<td><a href="index.php?route=local&action=alterar&codigo=<?=$local['id']?>">Alterar</a></td>
 				</tr>
 			<?php } ?>
 		</table>
