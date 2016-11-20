ccaae43e7b66f9e32c0867580f11c083504a634d
Tiago Henrique <Tiago Henrique@192.168.25.16>
2016-11-20 20:10:45 -0200

    Ajustado codigo

--- app/db_connection.php
+++ app/db_connection.php
@@ -1,18 +1,12 @@
 <?php
-$titulo = "Conexão com o Banco de Dados";
-
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "miranda_me1";
+$db_name = "miranda_me";
 
-function getConnection() {
-	$connection = @mysqli_connect ( $db_host, $db_user, $db_pass, $db_name );
+$connection = @mysqli_connect ( $db_host, $db_user, $db_pass, $db_name );
 
-	return $connection;
-}
-
-if (mysqli_connect_errno(getConnection())) {
+if (mysqli_connect_errno($connection)) {
 	$resultado = "Algum erro ocorreu com a conexão do servidor de
 			banco de dados, erro reportado: " . mysqli_connect_error ();
 } else {
