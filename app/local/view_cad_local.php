d53d69fcfa768180781cf70d481209ea77ca89b6
Tiago Henrique <Tiago Henrique@192.168.25.16>
2016-11-20 20:08:20 -0200

    Add return dos valores

--- app/local/view_cad_local.php
+++ app/local/view_cad_local.php
@@ -9,13 +9,13 @@
 	<?php if(isset($erro)) { ?>
 		<h2><?=$erro?></h2>
 	<?php } ?>
-	<h3><?=$resultado?></h3>
-	<form action="index.php?route=local&action=cadastrar" method="post" name="frmCadLocal">
-		<input type="hidden" name="txtId" />
+	<form action="index.php?route=local&action=cadastrar" method="post">
+		<input type="hidden" name="frmCadLocal" />
+		<input type="hidden" name="txtId" value="<?=@$dados['id']?>" />
 		<p>Nome:</p>
-		<p><input type="text" name="txtNome" /></p>
+		<p><input type="text" name="txtNome" value="<?=@$dados['nome']?>" /></p>
 		<p>Responsável:</p>
-		<p><input type="text" name="txtResponsavel" /></p>
+		<p><input type="text" name="txtResponsavel" value="<?=@$dados['responsavel']?>" /></p>
 		<p><input type="submit" value="Cadastrar" /></p>
 	</form>
 </body>
