f2dc464422d3f55a2d4aebe38b307cd74fd1893a
Tiago Henrique <Tiago Henrique@192.168.25.16>
2016-11-20 19:40:27 -0200

    Corrigido sql e add function

--- app/local/model_local.php
+++ app/local/model_local.php
@@ -3,24 +3,31 @@
 function salvar($connection, $id, $nome, $responsavel) {
 	
 	if ($id == null || $id == "") {
-		$sql = "INSERT INTO local(nome, responsavel) VALUES('.$nome.', '.$responsavel.')";
+		$sql = "INSERT INTO local(nome, responsavel) VALUES('$nome', '$responsavel')";
 		$result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
 	} else {
-		$sql = "UPDATE local l SET l.nome = '.$nome.', l.responsavel = '.$responsavel.' WHERE l.id = '.$id.'";
+		$sql = "UPDATE local l SET l.nome = '$nome', l.responsavel = '$responsavel' WHERE l.id = '$id'";
 		$result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
 	}
 	return $result;
 }
 
-function listaTodos($connection) {
+function pesquisaTodos($connection) {
 	$sql = "SELECT l.id, l.nome, l.responsavel FROM local l ORDER BY l.nome";
 	$result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
 	
 	return $result;
 }
 
-function pesquisaPorNome($connection, $nome) {
-	$sql = "SELECT l.nome, l.responsavel FROM local l WHERE l.nome = '.$nome.'";
+function pesquisaId($connection, $id) {
+	$sql = "SELECT l.id, l.nome, l.responsavel FROM local l WHERE l.id = '$id'";
+	$result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
+	
+	return $result;
+}
+
+function pesquisaNome($connection, $nome) {
+	$sql = "SELECT l.nome, l.responsavel FROM local l WHERE l.nome = '$nome'";
 	$result = mysqli_query($connection, $sql);
 	
 	return $result;
