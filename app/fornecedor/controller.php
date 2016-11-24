271ed9fbf8dc3e4c66522ed1c136ef9439b6adc4
Tiago Henrique <Tiago Henrique@192.168.25.16>
2016-11-24 12:54:11 -0200

    Add controller

--- app/fornecedor/controller.php
+++ app/fornecedor/controller.php
@@ -1,1 +1,26 @@
 <?php
+require_once 'db_connection.php';
+require_once 'functions.php';
+
+$connection = @mysqli_connect ( $db_host, $db_user, $db_pass, $db_name );
+
+if (isset ( $_GET ['action'] )) {
+	$acao = $_GET ['action'];
+} else {
+	$acao = null;
+}
+
+switch ($acao) {
+	case "register" :
+		registerProvider( $connection );
+		break;
+	
+	case "alter" :
+		alterProvider( $connection );
+		break;
+	
+	default :
+		$fornecedores = showAll( $connection );
+		require 'view_list.php';
+		break;
+}
