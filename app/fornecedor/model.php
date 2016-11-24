c641219776f856743497e40e74c0b42d80677f71
Tiago Henrique <Tiago Henrique@192.168.25.16>
2016-11-24 21:44:06 -0200

    Corrigido bug de sql

--- app/fornecedor/model.php
+++ app/fornecedor/model.php
@@ -7,7 +7,7 @@ function save($connection, $cnpj, $nome_fantasia, $razao_social, $inscricao_esta
 		$result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
 	} else {
 		$sql = "UPDATE fornecedor f ". 
-					 "SET f.nome_fantasia = '$nome_fantasia', f.razao_social = '$razao_social', f.inscricao_estadual = '$inscricao_estadual' ". 
+					 "SET f.nome_fantasia = '$nome_fantasia', f.razao_social = '$razao_social', f.inscricao_estadual = '$inscricao_estadual', ". 
 						  "f.endereco = '$endereco', f.telefone = '$telefone', f.uf = '$uf', f.municipio = '$municipio' ".
 					"WHERE f.cnpj = '$cnpj'";
 		$result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
