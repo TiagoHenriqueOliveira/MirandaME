b5e8ae3e184635eab153aa25b9709b167ea037d9
Tiago Henrique <Tiago Henrique@192.168.25.16>
2016-11-25 23:21:28 -0200

    Add index and model

---
+++ app/termo/transferencia/model.php
@@ -0,0 +1,38 @@
+<?php
+
+function salvar($connection, $id, $data, $responsavel_proximo, $id_responsavel_anterior) {
+	
+	if ($id == null || $id == "") {
+		$sql = "INSERT INTO termo_transferencia(data, responsavel_proximo, id_responsavel_anterior) ". 
+		 		  "VALUES('$data', '$responsavel_proximo', '$id_responsavel_anterior')";
+		$result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
+	} else {
+		$sql = "UPDATE termo_transferencia tt ".
+		          "SET tt.data = '$data', tt.responsavel_proximo = '$responsavel_proximo', tt.responsavel_anterior = '$id_responsavel_anterior' ".
+		          "WHERE tt.id = '$id'";
+		$result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
+	}
+	return $result;
+}
+
+function listAll($connection) {
+	$sql = "SELECT tt.id, tt.data, tt.responsavel_proximo, tr.responsavel ".
+			  "FROM termo_transferencia tt ".
+			  "INNER JOIN termo_responsabilidade tr ".
+			  "ON tt.id_responsavel_anterior = tr.id ".
+			  "ORDER BY tt.responsavel_proximo";
+	$result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
+	
+	return $result;
+}
+
+function searchId($connection, $id) {
+	$sql = "SELECT tt.id, tt.data, tt.responsavel_proximo, tr.responsavel ".
+			  "FROM termo_transferencia tt ".
+			  "INNER JOIN termo_responsabilidade tr ".
+			  "ON tt.id_responsavel_anterior = tr.id ".
+			  "WHERE tt.id = '$id'";
+	$result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
+	
+	return $result;
+}
