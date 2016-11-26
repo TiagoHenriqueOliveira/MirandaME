ec968d9cd9ef1bc7873fdf88fd095930afd1b9c5
Tiago Henrique <Tiago Henrique@192.168.25.16>
2016-11-25 23:21:12 -0200

    Add views list and register

---
+++ app/termo/transferencia/view_list.php
@@ -0,0 +1,45 @@
+<?php
+include '../header.php';
+?>
+<div id="page-wrapper">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1 class="page-header">Lista de Termos - Transferência</h1>
+				<ol class="breadcrumb">
+					<li><i class="glyphicon glyphicon-home"></i><a href="../principal.php"> Miranda ME</a></li>
+					<li class="active"><i class="fa fa-file-text fa-fw"></i> Termos</li>
+					<li class="active"><i class="fa fa-list-alt fa-fw"></i> Pesquisar Termos de Transferência</li>
+				</ol>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<table width="100%" class="table table-striped table-bordered table-hover"	id="dataTables-example">
+					<thead>
+						<tr>
+							<th><i class="fa fa-user fa-fw"></i> Responsável Anterior</th>
+							<th><i class="fa fa-user fa-fw"></i> Responsável Atual</th>
+							<th><i class="fa fa-calendar fa-fw"></i> Data</th>
+							<th><i class="fa fa-gear fa-fw"></i> Manutenção</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php foreach($termos_transferencias as $termo_transferencia) { ?>
+						<input type="hidden" name="txtId" value="<?=$termo_transferencia['id']?>" />
+						<tr class="odd gradeX">
+							<td><?=$termo_transferencia['responsavel_anterior']?></td>
+							<td><?=$termo_transferencia['responsavel_proximo']?></td>
+							<td><?=$termo_transferencia['data']?></td>
+							<td><a href="index.php?route=termo/responsabilidade&action=alter&codigo=<?=$termo_transferencia['id']?>" class="btn btn-primary">Alterar</a></td>
+						</tr>
+						<?php } ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+include '../footer.php';
+?>
---
+++ app/termo/transferencia/view_register.php
@@ -0,0 +1,52 @@
+<?php
+include '../header.php';
+?>
+<div id="page-wrapper">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1 class="page-header">Cadastro de Termos - Transferência</h1>
+				<ol class="breadcrumb">
+					<li><i class="glyphicon glyphicon-home"></i><a href="../principal.php"> Miranda ME</a></li>
+					<li class="active"><i class="fa fa-file-text fa-fw"></i> Termos </li>
+					<li class="active"><i class="fa fa-check fa-fw"></i> Cadastrar Termos de Transferência</li>
+				</ol>
+			</div>
+		</div>
+		<div class="panel panel-primary">
+			<div class="panel-body">
+				<div class="row">
+					<div class="col-xs-12">
+						<form role="form" action="index.php?route=termo/transferencia&action=register" method="post" accept-charset="utf-8">
+							<input type="hidden" name="frmCadTermoTransferencia" />
+							<input type="hidden" name="txtId" value="<?=@$dados['id']?>" />
+							<div class="form-group col-xs-12">
+								<label for="nomeResponsavelAnterior">Nome do Responsável Anterior:</label>
+								<select class="form-control" name="txtIdResponsavelAnterior" id="nomeResponsavelAnterior">
+								<?php foreach($responsaveis_anteriores as $responsavel_anterior) { ?>
+									<option value="<?=$responsavel_anterior['idTermoAnterior']?>"><?=$responsavel_anterior['responsavel_anterior']?></option>
+								<?php } ?>
+								</select>
+							</div>
+							<div	class="form-group col-xs-12">
+								<label for="nomeResponsavelAtual">Nome do Responsável Atual:</label>
+								<input class="form-control" type="text" id="nomeResponsavelAtual" name="txtResponsavelProximo" value="<?=@$dados['responsavelAtual']?>" maxlength="255" required />
+							</div>
+							<div	class="form-group col-xs-8 col-sm-6 col-md-4 col-lg-3">
+								<label for="data">Data:</label>
+								<input class="form-control" type="date" id="data" name="txtData" value="<?=@$dados['data']?>" maxlength="255" required />
+							</div>
+							<div class="form-group col-xs-12">
+								<button class="btn btn-success" type="submit">Salvar</button>
+								<button class="btn btn-warning" type="reset">Cancelar</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+include '../footer.php';
+?>
